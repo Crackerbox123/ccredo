9d419e93547c7d514191c74b41fbd9c8983a937b
Kal O <koestreicher4@gmail.com>
2022-10-24 06:58:21 -0500

    snapshot fix

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,8 +2,8 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/js/index.js',
   mode: 'development',
+  entry: './src/js/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
